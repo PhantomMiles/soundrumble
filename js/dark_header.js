19576a532d719783d2ebea0aed763a09b10c0e94
Your Name <you@example.com>
2022-06-08 17:05:53 +0100

    first commit

---
+++ js/dark_header.js
@@ -0,0 +1,28 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+var nav = document.querySelectorAll("[data-nav]  a");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        header.style.backgroundColor = "rgba(11, 11, 11, 0.83)";
+        // header.style.marginTop = "10px";
+        header.style.transition = "ease-in 0.2s";
+        nav.forEach(link => {
+            link.style.color = "white";
+        })
+    } else {
+        header.classList.remove("sticky");
+        header.style.backgroundColor = "transparent";
+        nav.forEach(link => {
+            link.style.color = "rgba(255, 255, 255, 0.704)";
+        })
+    }
+}
